f36fc563e07c7cc5241f88f5b56eacebabe4ba3e
CarlosCanuto <canutoc2007@gmail.com>
2026-09-12 12:36:23 +0100

    :sparkles: feat: add switch component

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,10 @@
+import "./switch.scss";
+
+export default function Switch() {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider"></span>
+    </label>
+  );
+}
